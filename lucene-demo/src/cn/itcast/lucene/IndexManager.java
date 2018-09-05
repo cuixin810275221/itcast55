b0d97d7fbc657beef205fdb8248879dbce81a63d
cx <cx810275221@163.com>
2018-09-05 16:46:32 +0800

    22

--- lucene-demo/src/cn/itcast/lucene/IndexManager.java
+++ lucene-demo/src/cn/itcast/lucene/IndexManager.java
@@ -91,7 +91,7 @@ public class IndexManager {
 		// 5.关闭资源
 		indexReader.close();
 	}
-
+//你好
 	@Test
 	public void deleteIndex() throws IOException {
 		FSDirectory directory = FSDirectory.open(new File("d:\\index"));
